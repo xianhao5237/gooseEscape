2408b649e8cd045eabc1b8f64dccfb2a6304823f
Xianhao Zhou <xianhaozhou@gmail.com>
2021-10-24 22:26:03 -0400

    Update gooseEscapeActors.hpp

--- gooseEscapeActors.hpp
+++ gooseEscapeActors.hpp
@@ -6,16 +6,14 @@
 #include <BearLibTerminal.h>
 #include "gooseEscapeUtil.hpp"
 
-class Actor
-{
+class Actor {
   private:
     int actorChar;      
     int location_x, location_y;
     bool shield;
 
   public:
-    Actor()
-    {
+    Actor() {
         actorChar = int('A');
         location_x = MIN_SCREEN_X;
         location_y = MIN_SCREEN_Y;
@@ -23,8 +21,7 @@ class Actor
         put_actor();
     }
 
-    Actor(char initPlayerChar, int x0, int y0)
-    {
+    Actor(char initPlayerChar, int x0, int y0) {
         change_char(initPlayerChar);
         location_x = MIN_SCREEN_X;
         location_y = MIN_SCREEN_Y;
@@ -32,18 +29,15 @@ class Actor
         update_location(x0,y0);
     }
     
-    int get_x() const
-    {
+    int get_x() const {
         return location_x;
     }
     
-    int get_y() const
-    {
+    int get_y() const {
         return location_y;
     }
     
-    string get_location_string() const
-    {
+    string get_location_string() const {
         char buffer[80];
         itoa(location_x,buffer,10);
         string formatted_location = "(" + string(buffer) + ",";
@@ -52,13 +46,11 @@ class Actor
         return formatted_location;
     }
     
-    void change_char(char new_actor_char)
-    {
+    void change_char(char new_actor_char) {
         actorChar = min(int('~'),max(int(new_actor_char),int(' ')));
     }
 
-    bool can_move(int delta_x, int delta_y) const
-    {
+    bool can_move(int delta_x, int delta_y) const {
         int new_x = location_x + delta_x;
         int new_y = location_y + delta_y;
 
@@ -66,8 +58,7 @@ class Actor
           && new_y >= MIN_BOARD_Y && new_y <= MAX_BOARD_Y;
     }
 
-    void update_location(int delta_x, int delta_y)
-    {
+    void update_location(int delta_x, int delta_y) {
         if (can_move(delta_x, delta_y))
         {
             terminal_clear_area(location_x, location_y, 1, 1);
@@ -83,8 +74,7 @@ class Actor
         terminal_refresh();
     }
     
-    void set_location(int xCoord, int yCoord)
-    {
+    void set_location(int xCoord, int yCoord) {
         terminal_clear_area(location_x, location_y, 1, 1);
         location_x = xCoord;
         location_y = yCoord;
@@ -92,13 +82,11 @@ class Actor
     }
     
     // check if player has a shield
-	bool isShield() const
-    {
+	bool isShield() const {
         return shield;
     }
 	// add or remove shield
-    void changeShield(bool shieldChange)
-    {
+    void changeShield(bool shieldChange) {
         shield = shieldChange;
     }
     
